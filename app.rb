e5365ce811fbf263c53c8d03b6c6cac8fbff5d7a
Taylor Williams <taylor@thewilliams.net>
2018-08-12 01:29:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,6 +22,8 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    "#{@num1 * @num2}" if @num1.is_a?(Integer) && @num2.is_a?(Integer)
+    if @num1.is_a?(Integer) && @num2.is_a?(Integer)
+      "#{@num1 * @num2}"
+    end
   end
 end
